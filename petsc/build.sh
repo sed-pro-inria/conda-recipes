7cd4c8589181f8145149dcafae30e153d843a75a
David Froger <david.froger@inria.fr>
2015-10-02 23:52:16 +0200

    Add petsc recipe

---
+++ petsc/build.sh
@@ -0,0 +1,20 @@
+export PETSC_DIR=$PWD
+export PETSC_ARCH=linux-64-release
+
+python config/configure.py \
+    --prefix=$PREFIX \
+    --with-shared-libraries \
+    --with-blas-lapack-lib=openblas \
+    --with-mpi-dir=$PREFIX \
+    --with-metis-dir=$PREFIX \
+    --with-scalapack-dir=$PREFIX \
+    --with-parmetis-dir=$PREFIX \
+    --with-mumps-dir=$PREFIX \
+    --with-superlu_dist-dir=$PREFIX \
+    --with-suitesparse-dir=$PREFIX
+
+make all
+
+make install
+
+# todo: mpi4py, petsc4py fiat, fftw, boost
