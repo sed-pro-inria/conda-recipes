bced0f874ecdaba59be7aaea12b2e40a6cb84c5d
David Froger <david.froger@inria.fr>
2015-10-13 09:58:33 +0200

    Add mpi4py recipe

---
+++ mpi4py/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -o errexit
+set -o pipefail
+
+if [ -z "$OSX_ARCH" ]
+then
+    :
+else
+    export CC=clang
+    export CXX=clang
+
+    export CFLAGS='-arch x86_64'
+    export CXXFLAGS='-arch x86_64'
+
+    export LDFLAGS="-arch x86_64 -L$PREFIX/lib"
+fi
+
+python setup.py build --mpicc=$PREFIX/bin/mpicc
+python setup.py install --prefix=$PREFIX
