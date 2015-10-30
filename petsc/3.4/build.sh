8172029c820cc00c4f4edc0e8b66fdea25c49b9f
David Froger <david.froger@inria.fr>
2015-10-30 10:08:27 +0100

    PETSc: add config for OSX

--- petsc/3.4/build.sh
+++ petsc/3.4/build.sh
@@ -1,16 +1,30 @@
 export PETSC_DIR=$PWD
 export PETSC_ARCH=linux-64-release
 
-python config/configure.py \
-    --prefix=$PREFIX \
-    --with-shared-libraries \
-    --with-blas-lapack-lib=openblas \
-    --with-mpi-dir=$PREFIX \
-    --with-metis-dir=$PREFIX \
-    --with-scalapack-dir=$PREFIX \
-    --with-parmetis-dir=$PREFIX \
-    --with-mumps-dir=$PREFIX \
-    --with-superlu_dist-dir=$PREFIX
+if [ "$(uname)" == "Darwin" ];
+then
+    python config/configure.py \
+        --prefix=$PREFIX \
+        --with-shared-libraries \
+        --with-mpi-dir=$PREFIX \
+        --with-metis-dir=$PREFIX \
+        --with-scalapack-dir=$PREFIX \
+        --with-parmetis-dir=$PREFIX \
+        --with-mumps-dir=$PREFIX \
+        --with-superlu_dist-dir=$PREFIX \
+        --LDFLAGS="-Wl,-rpath,$PREFIX/lib"
+else
+    python config/configure.py \
+        --prefix=$PREFIX \
+        --with-shared-libraries \
+        --with-blas-lapack-lib=openblas \
+        --with-mpi-dir=$PREFIX \
+        --with-metis-dir=$PREFIX \
+        --with-scalapack-dir=$PREFIX \
+        --with-parmetis-dir=$PREFIX \
+        --with-mumps-dir=$PREFIX \
+        --with-superlu_dist-dir=$PREFIX
+fi
 
 make all
 
