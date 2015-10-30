2dc861d94a2a72d0e2e9664edbe7fa74b3c420da
David Froger <david.froger@inria.fr>
2015-10-30 10:24:04 +0100

    lm5: dylib extension on OSX

--- lm5/build.sh
+++ lm5/build.sh
@@ -1,10 +1,17 @@
+if [ "$(uname)" == "Darwin" ];
+then
+    EXT=dylib
+else
+    EXT=so
+fi
+
 gcc \
     -I. -fno-common -fPIC -shared \
-    -o libmesh5.so \
+    -o libmesh5.$EXT \
     libmesh5.c
 
 mkdir -p $PREFIX/lib
 mkdir -p $PREFIX/include
 
-cp libmesh5.so $PREFIX/lib
+cp libmesh5.$EXT $PREFIX/lib
 cp libmesh5.h $PREFIX/include
