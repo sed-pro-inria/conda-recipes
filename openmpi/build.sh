46db929acd50401d2ff2887c414c90b9d76acb2a
David Froger <david.froger@inria.fr>
2015-10-30 08:33:44 +0100

    OpenMPI: turn off flags for recent gcc

--- openmpi/build.sh
+++ openmpi/build.sh
@@ -1,7 +1,8 @@
 # For CFLAGS=-fgnu89-inline, see:
 # http://lifecs.likai.org/2009/06/multiple-definition-of-extern-inline.html
 
-ln -s $PREFIX/lib $PREFIX/lib64
+# ln -s $PREFIX/lib $PREFIX/lib64
+# CFLAGS=-fgnu89-inline
 
 ./configure \
     --prefix=$PREFIX \
@@ -9,8 +10,7 @@ ln -s $PREFIX/lib $PREFIX/lib64
     --enable-mpi-fortran \
     --enable-mpi-cxx \
     --enable-shared=yes \
-    --enable-static=yes \
-    CFLAGS=-fgnu89-inline
+    --enable-static=yes
 
 make -j $CPU_COUNT
 
