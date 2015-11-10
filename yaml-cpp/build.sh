8bb50ff9184e52eb3611e28e602e907bdcc2f48c
David Froger <david.froger@inria.fr>
2015-11-10 06:48:48 -0800

    Build yaml-cpp with clang on OSX

--- yaml-cpp/build.sh
+++ yaml-cpp/build.sh
@@ -1,6 +1,13 @@
+export MACOSX_DEPLOYMENT_TARGET=10.7
+
 mkdir build
 cd build
-cmake \
+VERBOSE=1 cmake \
+    -DCMAKE_C_COMPILER=clang \
+    -DCMAKE_C_FLAGS="-mmacosx-version-min=10.7" \
+    -DCMAKE_CXX_COMPILER=clang++ \
+    -DCMAKE_CXX_FLAGS="-stdlib=libc++ -mmacosx-version-min=10.7" \
+    -DCMAKE_SHARED_LINKER_FLAGS="-stdlib=libc++ -mmacosx-version-min=10.7" \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
     -DBoost_INCLUDE_DIR=$PREFIX/include \
     -DBUILD_SHARED_LIBS=ON \
