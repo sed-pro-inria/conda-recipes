648488c468f2dcffc961ce4c692b7c7bd8b1ce01
David Froger <david.froger@inria.fr>
2015-11-19 12:46:47 +0100

    Add tuto-reloc

---
+++ tuto-reloc/src/greet.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "greet.h"
+
+void
+greet()
+{
+    printf("Hello, this is an hardcoded path in binary file: /home/david/tuto-reloc\n");
+}
---
+++ tuto-reloc/src/greet.h
@@ -0,0 +1,6 @@
+#ifndef GREET_H_INCLUDED
+#define GREET_H_INCLUDED
+
+void greet();
+
+#endif
